95293f0e9da0dc7accbf28b7b3321b44c7be5c32
pjastr <24464854+pjastr@users.noreply.github.com>
2017-09-27 18:01:08 +0200

    Add files via upload

---
+++ Ex101/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pierwszaLiczba = 10, drugaLiczba = 20, i, j, k;
+            Console.WriteLine("pierwszaLiczba: " + pierwszaLiczba);
+            Console.WriteLine("drugaLiczba: " + drugaLiczba);
+            Console.WriteLine("zmienna i: " + i);
+            Console.WriteLine("zmienna j: " + j);
+            Console.WriteLine("zmienna k: " + k);
+        }
+    }
+}
